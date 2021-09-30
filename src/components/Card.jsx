96ab757ab9b85da0167ec977c82f11439f8c8dd9
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-30 18:55:48 -0300

    Cria form dinamico e calculo base

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 export default class Card extends Component {
   render() {
     return (
-      <main class="main-content container py-3">
-        <div class="columns is-mobile is-centered p-5">
-          <div class="column is-four-fifths box p-5">
-            <p class="is-size-3 has-text-centered has-text-black p-2 mb-6">
+      <main className="main-content container py-3">
+        <div className="columns is-mobile is-centered p-5">
+          <div className="column is-four-fifths box p-5">
+            <p className="is-size-3 has-text-centered has-text-black p-2 mb-6">
               {this.props.name}
             </p>
             {this.props.children}
--- src/components/forms/Despesa.jsx
+++ src/components/forms/Despesa.jsx
@@ -1,36 +1,65 @@
-import React, { Component } from "react";
-import Card from "../Card";
-import { nanoid } from "nanoid";
-const inputs = ["Aereo", "AlomocoCliente", "AlmocoMotorista"];
+import React, { Component } from 'react';
+import Card from '../Card';
+import { nanoid } from 'nanoid';
+const inputs = ['Aereo', 'AlomocoCliente', 'AlmocoMotorista'];
 export default class Despesa extends Component {
+  constructor() {
+    super();
+    this.state = inputs.reduce((a, v) => ({ ...a, [v]: 0 }), {});
+    console.log(this.state);
+    this.handleChange = this.handleChange.bind(this);
+    this.sum = this.sum.bind(this);
+  }
+  handleChange({ target }) {
+    let input = target.name.split(/(?=[A-Z])/);
+    input =
+      input.length > 2 ? input.slice(1 - input.length).join('') : input.pop();
+    this.setState((previousState, _props) => {
+      previousState[target.name] = parseFloat(target.value);
+    });
+    this.sum(input);
+  }
+  sum(input) {
+    this.setState((previousState, _props) => {
+      previousState[`total${input}`] =
+        previousState[`valor${input}`] * previousState[`quantidade${input}`];
+      document.getElementById(`total${input}`).value =
+        previousState[`total${input}`];
+    });
+  }
   render() {
     return (
       <Card name="Despesa">
-        {inputs.map((input) => (
-          <div className="field columns">
+        {inputs.map((input, index) => (
+          <div className="field is-horizontal columns" key={nanoid()}>
+            <div className="field-label has-text-left column is-3">
+              <label className="label " htmlFor="1">
+                {input}
+              </label>
+            </div>
             <div className="field-body">
-              <div className="field-label column is-one-fifth has-text-left">
-                <label className="label" htmlFor="1">
-                  {input}
-                </label>
-              </div>
               <input
                 type="number"
-                className="input column is-6 mr-2"
+                className="input column is-7 mr-2"
                 name={`valor${input}`}
-                id="1"
+                id={index}
+                value={this.state.input}
+                onChange={this.handleChange}
               />
               <input
                 type="number"
-                className="input is-1 mr-2"
+                className="input mr-2"
                 name={`quantidade${input}`}
-                id="3"
+                id={index}
+                value={this.state.input}
+                onChange={this.handleChange}
               />
               <input
                 type="number"
                 className="input mr-2"
                 name={`total${input}`}
-                id="4"
+                id={`total${input}`}
+                value={this.state.input}
                 disabled
               />
             </div>
