2cde88a402246ba9550ddfd2860fd8d0b6336981
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-10-01 00:29:20 -0300

    Cria inputs com base no retorno do api

--- src/components/forms/Despesa.jsx
+++ src/components/forms/Despesa.jsx
@@ -1,14 +1,35 @@
 import React, { Component } from 'react';
 import Card from '../Card';
 import { nanoid } from 'nanoid';
+import getDespesas from '../../services/getDespesas';
+
 const inputs = ['Aereo', 'AlomocoCliente', 'AlmocoMotorista'];
+
 export default class Despesa extends Component {
   constructor() {
     super();
-    this.state = inputs.reduce((a, v) => ({ ...a, [v]: 0 }), {});
-    console.log(this.state);
+    // this.state = inputs.reduce((a, v) => ({ ...a, [v]: 0 }), {});
+    this.state = { inputName: [], states: {} };
     this.handleChange = this.handleChange.bind(this);
     this.sum = this.sum.bind(this);
+  }
+  componentDidMount() {
+    this.getApiData();
+  }
+  async getApiData() {
+    // .reduce((res, key) => (res[key] = obj[key]), {});
+    const despesas = await getDespesas();
+    const getAndOrderKeys = (obj) =>
+      Object.keys(obj)
+        .sort((a, b) => (a < b ? 1 : -1))
+        .filter((obj) => !obj.includes('id'))
+        .map((obj) => obj.split('valor').pop());
+    const keys = getAndOrderKeys(despesas);
+    this.setState({
+      inputName: getAndOrderKeys(despesas),
+      states: Object.assign({}, ...keys.map((key) => ({ [key]: 0 }))),
+    });
+    console.log(Object.keys(this.state.states).length);
   }
   handleChange({ target }) {
     let input = target.name.split(/(?=[A-Z])/);
@@ -27,10 +48,12 @@ export default class Despesa extends Component {
         previousState[`total${input}`];
     });
   }
+
   render() {
     return (
       <Card name="Despesa">
-        {inputs.map((input, index) => (
+        {console.log()}
+        {this.state.inputName.map((input, index) => (
           <div className="field is-horizontal columns" key={nanoid()}>
             <div className="field-label has-text-left column is-3">
               <label className="label " htmlFor="1">
---
+++ src/services/getDespesas.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const data = { id: 70 };
+const formData = new FormData();
+for (const key in data) {
+  formData.append(key, data[key]);
+}
+const sendRequest = async () => {
+  try {
+    const resp = await axios.post('http://localhost/Projetos/SistemaFabio-2.0/teste/findDespesa.php', formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+    const {data: {despesa}} = resp
+    return despesa;
+  } catch (err) {
+    return err;
+  }
+}
+export default sendRequest;
