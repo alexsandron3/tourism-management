c66151eb0a4ced53f775e496c7a4e0c350ebbfe5
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-10-10 18:37:17 -0300

    Adiciona toolbar com export excel e pdf

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
-import { Grid, TextField } from '@mui/material';
+import { Button, Grid, IconButton, TextField } from '@mui/material';
 import Appbar from '../Appbar';
 import Content from '../partials/Content';
-import { DataGrid, ptBR } from '@mui/x-data-grid';
+import {
+  DataGrid,
+  ptBR,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarDensitySelector,
+} from '@mui/x-data-grid';
+import LocalPrintshopIcon from '@mui/icons-material/LocalPrintshop';
 import 'date-fns';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import moment from 'moment';
 import axios from 'axios';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 const columns = [
   {
     field: 'idPasseio',
@@ -91,19 +101,17 @@ const columns = [
   //     }`,
   // },
 ];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+const col = [
+  'ID',
+  'Passeio',
+  'Data',
+  'Reservados',
+  'Interessados',
+  'Parceiros',
+  'Crianças',
+  'Meta de vendas',
 ];
-//
+
 export default class Index extends Component {
   constructor() {
     super();
@@ -116,6 +124,44 @@ export default class Index extends Component {
   componentDidMount() {
     this.fetchData();
   }
+  handleClick = () => {
+    const doc = new jsPDF();
+    doc.text('RELATÓRIO GERENCIAL DE VENDAS', 20, 10);
+
+    // doc.autoTable({ html: '.opa' });
+    const a = this.state.row.map((column) => Object.values(column));
+    doc.autoTable({
+      columns: [...col],
+
+      body: [...a],
+    });
+    // const a = columns.map((column) => ({
+    //   // ...column,
+    //   dataKey: column.headerName,
+    // }));
+    console.log(a);
+    doc.save('table.pdf');
+  };
+  CustomToolbar = () => {
+    return (
+      <GridToolbarContainer xs={{ marginBottom: 100 }}>
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+
+        <IconButton
+          size="large"
+          edge="start"
+          color="primary"
+          aria-label="menu"
+          onClick={() => this.handleClick('appBarOpened', true)}
+        >
+          {/* <GridToolbarExport /> */}
+          <LocalPrintshopIcon />
+        </IconButton>
+      </GridToolbarContainer>
+    );
+  };
 
   fetchData = async () => {
     // const { startDate, endDate } = this.state;
@@ -137,40 +183,70 @@ export default class Index extends Component {
       const {
         data: { passeios },
       } = answer;
-      // const payments = Object.entries(answer.data[0]);
-      // console.log(typeof passeios.passeio);
-      // console.log(passeios);
-
+      // const pagamentos = passeios[0].reduce(
+      //   (acc, curr) => {
+      //     const pagamentos = acc;
+      //     if (curr.statusPagamento === 0) acc.interessados += 1;
+      //     if (curr.statusPagamento === 1) acc.quitados += 1;
+      //     if (curr.statusPagamento === 2) acc.confirmados += 1;
+      //     if (curr.statusPagamento === 3) acc.parceiros += 1;
+      //     if (curr.statusPagamento === 4) acc.criancas += 1;
+      //     return pagamentos;
+      //   },
+      //   {
+      //     interessados: 0,
+      //     quitados: 0,
+      //     confirmados: 0,
+      //     parceiros: 0,
+      //     criancas: 0,
+      //   }
+      // );
+      // passeios[0].forEach((p) =>
+      //   p.pagamentos.reduce(
+      //     (acc, curr) => {
+      //       const pagamentos = acc;
+      //       if (curr.statusPagamento === 0) acc.interessados += 1;
+      //       if (curr.statusPagamento === 1) acc.quitados += 1;
+      //       if (curr.statusPagamento === 2) acc.confirmados += 1;
+      //       if (curr.statusPagamento === 3) acc.parceiros += 1;
+      //       if (curr.statusPagamento === 4) acc.criancas += 1;
+      //       return pagamentos;
+      //     },
+      //     {
+      //       interessados: 0,
+      //       quitados: 0,
+      //       confirmados: 0,
+      //       parceiros: 0,
+      //       criancas: 0,
+      //     }
+      //   )
+      // );
+      // const a = passeios[0].map((p) =>
+      //   p.pagamentos.reduce(
+      //     (acc, curr) => {
+      //       const pagamentos = acc;
+      //       if (curr.statusPagamento === 0) acc.interessados += 1;
+      //       if (curr.statusPagamento === 1) acc.quitados += 1;
+      //       if (curr.statusPagamento === 2) acc.confirmados += 1;
+      //       if (curr.statusPagamento === 3) acc.parceiros += 1;
+      //       if (curr.statusPagamento === 4) acc.criancas += 1;
+      //       return pagamentos;
+      //     },
+      //     {
+      //       interessados: 0,
+      //       quitados: 0,
+      //       confirmados: 0,
+      //       parceiros: 0,
+      //       criancas: 0,
+      //     }
+      //   )
+      // );
       this.setState({
         row: passeios[0],
       });
-      // console.log(Object.entries(payments));
-      // const payments = await Promise.all(
-      //   passeios.map(async (passeio) => {
-      //     // const arr = payments.filter((payment) => payment[0] == passeio.idPasseio);
-      //     // return http://localhost/Projetos/SistemaFabio-2.0/api/pagamento.php?id=27
-      //     const pp = await axios({
-      //       method: 'GET',
-      //       url: `http://localhost/Projetos/SistemaFabio-2.0/api/pagamento.php?id=${passeio.idPasseio}`,
-      //     });
-      //     return pp.data;
-      //   })
-      // );
-      // payments.reduce((payment) => {
-      //   const [status] = Object.keys(payment.data.pagamentos);
-      // });
-      // payments.map((payment) => {
-      //   const status = Object.keys(payment.data.pagamentos);
-      // });
-      // console.log(...this.state.row);
-      // console.log(payments[0].data.pagamentos);
-      // console.log(passeios[0].idPasseio, payments[13]);
-      // const newObj = Object.assign(
-      //   {},
-      //   ...this.state.row.map((item) => ({ [item.key]: item.value }))
-      // );
+      console.log(passeios[0]);
 
-      console.log(this.state.row);
+      // console.log(pagamentos);
     } catch (err) {
       console.error(err);
     }
@@ -223,15 +299,17 @@ export default class Index extends Component {
                   />
                 </LocalizationProvider>
               </Grid>
-              <div style={{ height: 400, width: '100%' }}>
+              <div style={{ height: 700, width: '100%' }}>
                 <DataGrid
+                  components={{
+                    Toolbar: this.CustomToolbar,
+                  }}
+                  className="opa"
                   localeText={ptBR.props.MuiDataGrid.localeText}
                   rows={this.state.row}
                   // onClick={console.log(this.state.row)}
                   getRowId={(row) => row.idPasseio}
                   columns={columns}
-                  // pageSize={5}
-                  // rowsPerPageOptions={[5]}
                   disableSelectionOnClick
                 />
               </div>
